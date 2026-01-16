ffcd2aef6d03b1a6c4e9135515d3e9792bff84eb
Sanskruti Shelke <sans.shelkee@gmail.com>
2026-01-16 09:53:27 +0530

    adding modified files after final review

--- blog_project/accounts/forms.py
+++ blog_project/accounts/forms.py
@@ -3,37 +3,44 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=False)
     last_name = forms.CharField(max_length=30, required=False)
-    
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
-    
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2'
+        ]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_name in self.fields:
-            self.fields[field_name].widget.attrs.update({'class': 'form-control'})
-    
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        
-        # ⭐⭐ CRITICAL FIX: ACTIVATE USER ACCOUNT ⭐⭐
+        user.first_name = self.cleaned_data.get('first_name', '')
+        user.last_name = self.cleaned_data.get('last_name', '')
         user.is_active = True
-        
+
         if commit:
             user.save()
         return user
 
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['bio', 'website', 'location', 'profile_picture']
+        fields = ['bio', 'website', 'location']
         widgets = {
             'bio': forms.Textarea(attrs={
                 'class': 'form-control',
